bac3f15caedabba5a6d6ee490ad3d1ca6b63709c
EleandroSilva <eleandrobemoreweb@gmail.com>
2022-09-19 13:43:39 -0300

    19.09.2022 13:43

--- ServidorDataSnapRest/DataSnapREST/source/MVC/Model/Entidades/Interfaces/Model.Entidade.Pessoa.Interfaces.pas
+++ ServidorDataSnapRest/DataSnapREST/source/MVC/Model/Entidades/Interfaces/Model.Entidade.Pessoa.Interfaces.pas
@@ -8,8 +8,10 @@ uses
 type
   iEntidadePessoa = interface
     ['{843CDC82-FB1F-41C0-9B1B-F9CA1BA28EF4}']
-    function Get(const Key: String): TJsonArray;
-    function Put(const Key : String; jObject : TJsonObject) : iEntidadePessoa;
+    function Get(const    Key : String)                        : TJsonArray;
+    function Put(const    Key : String; jObject : TJsonObject) : iEntidadePessoa;
+    function Post(const   Key : String; jObject : TJsonObject) : iEntidadePessoa;
+    function Delete(const Key : String)                        : iEntidadePessoa;
   end;
 
 implementation
--- ServidorDataSnapRest/DataSnapREST/source/MVC/View/Entidades/ServerMethodsUnit1.dfm
+++ ServidorDataSnapRest/DataSnapREST/source/MVC/View/Entidades/ServerMethodsUnit1.dfm
@@ -2,4 +2,10 @@ object ServerMethods1: TServerMethods1
   OldCreateOrder = False
   Height = 451
   Width = 521
+  object ClientDataSet1: TClientDataSet
+    Aggregates = <>
+    Params = <>
+    Left = 248
+    Top = 208
+  end
 end
